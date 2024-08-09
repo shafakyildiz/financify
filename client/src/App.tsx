77200229022f40ed711ebeb933e04324d2fb49b5
shafakyildiz <shafakyildiz@gmail.com>
2024-08-09 11:04:04 +0300

    add theme provider and css baseline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline } from "@mui/material";
+
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
