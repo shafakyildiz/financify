eb6d74c45a263862af28a8b6fbd9e3e1cd3a6bc5
shafakyildiz <shafakyildiz@gmail.com>
2024-08-09 11:50:19 +0300

    feat: add dashboard component to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { Box, CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "@/scenes/navbar/index";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,7 +16,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route
                 path="/predictions"
                 element={<div>predictions page</div>}
