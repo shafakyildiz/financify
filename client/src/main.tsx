ed8fc85c44314a936ff1845bce6922e23ae7b238
shafakyildiz <shafakyildiz@gmail.com>
2024-08-10 11:51:20 +0300

    fix: organize imports on main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
 import "@/index.css";
-import { Provider } from "react-redux";
+import { api } from "@/state/api";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { api } from "@/state/api";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
