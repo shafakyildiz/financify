de1722a3c566a95ddbe3063580968c4a5203aeb8
shafakyildiz <shafakyildiz@gmail.com>
2024-08-10 11:33:28 +0300

    fix: change boxes with dashboard boxes

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import DashboardBox from "@/components/DashboardBox";
 import { Box, useMediaQuery, useTheme } from "@mui/material";
 
 type Props = {};
@@ -70,16 +71,16 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <Box bgcolor="#fff" gridArea="a"></Box>
-      <Box bgcolor="#fff" gridArea="b"></Box>
-      <Box bgcolor="#fff" gridArea="c"></Box>
-      <Box bgcolor="#fff" gridArea="d"></Box>
-      <Box bgcolor="#fff" gridArea="e"></Box>
-      <Box bgcolor="#fff" gridArea="f"></Box>
-      <Box bgcolor="#fff" gridArea="g"></Box>
-      <Box bgcolor="#fff" gridArea="h"></Box>
-      <Box bgcolor="#fff" gridArea="i"></Box>
-      <Box bgcolor="#fff" gridArea="j"></Box>
+      <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="d"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="e"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="f"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="g"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="h"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="i"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="j"></DashboardBox>
     </Box>
   );
 };
