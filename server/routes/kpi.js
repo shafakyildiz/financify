b1225035e0e6aa892eb845d19de0003f96216a3a
shafakyildiz <shafakyildiz@gmail.com>
2024-08-13 20:05:34 +0300

    feat: add kpi route

---
+++ server/routes/kpi.js
@@ -0,0 +1,14 @@
+import express from "express";
+import KPI from "../models/KPI";
+
+const router = express.Router();
+
+router.get("/kpis", async (req, res) => {
+  try {
+    const kpis = await KPI.find();
+  } catch (error) {
+    res.status.json({ message: error.message });
+  }
+});
+
+export default kpiRoutes;
