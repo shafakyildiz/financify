7f1e72f3fffdb9614f6d86d49294525f110a49cb
shafakyildiz <shafakyildiz@gmail.com>
2024-08-17 12:36:56 +0300

    fix: width and height problem by adding responsivecontainer from recharts

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -2,7 +2,14 @@ import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
 import { useTheme } from "@mui/material";
 import { useMemo } from "react";
-import { Area, AreaChart, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Row1 = () => {
   const { data } = useGetKpisQuery();
@@ -25,53 +32,59 @@ const Row1 = () => {
   return (
     <>
       <DashboardBox bgcolor="#fff" gridArea="a">
-        <AreaChart
-          width={500}
-          height={400}
-          data={revenueExpenses}
-          margin={{ top: 15, right: 0, left: -10, bottom: 60 }}
-        >
-          <defs>
-            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
-              <stop
-                offset="5%"
-                stopColor={palette.primary[300]}
-                stopOpacity={0.5}
-              />
-              <stop
-                offset="95%"
-                stopColor={palette.primary[300]}
-                stopOpacity={0}
-              />
-            </linearGradient>
-            <linearGradient id="colorExpense" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }} />
-          <YAxis
-            tickLine={false}
-            axisLine={{ strokeWidth: "0" }}
-            style={{ fontSize: "10px" }}
-            domain={[8000, 23000]}
-          />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="revenue"
-            stroke={palette.primary.main}
-            fillOpacity={1}
-            fill="url(#colorRevenue)"
-          />
-          <Area
-            type="monotone"
-            dataKey="expenses"
-            stroke={palette.secondary.main}
-            fillOpacity={1}
-            fill="url(#colorExpense)"
-          />
-        </AreaChart>
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={revenueExpenses}
+            margin={{ top: 15, right: 25, left: -10, bottom: 60 }}
+          >
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+              <linearGradient id="colorExpense" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              tickLine={false}
+              axisLine={{ strokeWidth: "0" }}
+              style={{ fontSize: "10px" }}
+              domain={[8000, 23000]}
+            />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue)"
+            />
+            <Area
+              type="monotone"
+              dataKey="expenses"
+              stroke={palette.secondary.main}
+              fillOpacity={1}
+              fill="url(#colorExpense)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
       </DashboardBox>
       <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
       <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
