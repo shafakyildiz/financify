1674442586271e48c4e9323673681063b23fd68a
shafakyildiz <shafakyildiz@gmail.com>
2024-08-18 11:29:25 +0300

    feat: implement GetTransactionsResponse type

--- client/src/state/types.ts
+++ client/src/state/types.ts
@@ -44,3 +44,14 @@ export interface GetProductsResponse {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface GetTransactionsResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  buyer: string;
+  amount: number;
+  productIds: Array<string>;
+  createdAt: string;
+  updatedAt: string;
+}
